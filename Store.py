a2bcb3a328c428769eef9e9ad24b12218639c232
Aiman Ismail <aimanismail15@gmail.com>
2020-03-13 13:31:06 -0700

    added some docstrings to store.py.

--- Store.py
+++ Store.py
@@ -15,11 +15,23 @@ class Store:
     pass
 
 
-
-
-class DailyTransactionReport():
+class OrderProcessor:
+    """
+    An Order Processor is an object that is responsible for reading a order
+    file to create an Order object.
+    """
     pass
 
-class ProcessWebOrders():
-    pass
-
+class Order:
+    """
+    An Order is an analogy for an business transaction that orders a variety
+    of items with appropriate business attributes like order number,
+    product ids.
+    """
+#  todo Add functionality
+# •	Order number
+# •	Product ID
+# •	Item - The type of item (Toy, StuffedAnimal and Candy).
+# •	Name of the item
+# •	A dictionary of product details. These details are the rest of the attributes of the item as specified in the excel sheet EXCEPT the name of the holiday — Easter, Christmas or Halloween.
+# •	The order should also contain a reference to the appropriate Factory object that can create this item.
