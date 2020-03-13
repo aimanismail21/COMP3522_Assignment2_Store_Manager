38bc6060b320001964c4b8d983ca5abe98e03803
Aiman Ismail <aimanismail15@gmail.com>
2020-03-13 14:04:52 -0700

    Created daily_transaction_report function Added some comments for requirements.

--- Store.py
+++ Store.py
@@ -6,12 +6,36 @@ Store contains the methods and classes responsible for:
 
 """
 
+
+
 # todo Special Requirements
-# The first time the store receives an order for an item, it is likely that it won't have the item in it's inventory since the store should be initialized with an empty inventory.
-# In the event the store receives an order for an item that it does not have inventory for, then it should go ahead and get a 100 of those items made by the corresponding factory class.
+# The first time the store receives an order for an item, it is likely that it won't
+# have the item in it's inventory since the store should be initialized with an empty inventory.
+# In the event the store receives an order for an item that it does not have inventory for,
+# then it should go ahead and get a 100 of those items made by the corresponding factory class.
 
 
 class Store:
-    pass
+    def __init__(self):
+        # inventory of items
+        # record of orders (list)
+        pass
 
+    def read_order(self):
+        pass
+
+    def order_items(self, item_factory):
+        """
+        Creates more items from a factory class
+        :param item_factory: a factory to produce a type of item
+        :return: an item
+        """
+        # This function needs to create 100 items of a specific type.
+        # We can yield these items until we have 100.
+
+    def daily_transaction_report(self):
+        # create a text file
+        # text file is a list of recorded orders in the day
+        # convention
+        pass
 
