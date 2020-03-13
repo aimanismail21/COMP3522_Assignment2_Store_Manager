cccd2fe574afa93bb2425de75d287486932809ac
Aiman Ismail <aimanismail15@gmail.com>
2020-03-13 13:00:33 -0700

    Added some docstrings and empty classes.

--- Store.py
+++ Store.py
@@ -1,5 +1,25 @@
 """
 Store contains the methods and classes responsible for:
 1. Receiving orders and maintainingits inventory
-2.Getting items from a factory class if the store does not have enough stock.
+2. Getting items from a factory class if the store does not have enough stock.
+3. Creating Daily Transaction Report
+
 """
+
+# todo Special Requirements
+# The first time the store receives an order for an item, it is likely that it won't have the item in it's inventory since the store should be initialized with an empty inventory.
+# In the event the store receives an order for an item that it does not have inventory for, then it should go ahead and get a 100 of those items made by the corresponding factory class.
+
+
+class Store:
+    pass
+
+
+
+
+class DailyTransactionReport():
+    pass
+
+class ProcessWebOrders():
+    pass
+
