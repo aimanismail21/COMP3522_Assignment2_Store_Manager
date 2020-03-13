a202491e235b789199c0606b27028a516fab3bd5
Aiman Ismail <aimanismail15@gmail.com>
2020-03-13 13:39:41 -0700

    Excised Order related code from Store.py into OrderProcessor.py.

---
+++ OrderProcessor.py
@@ -0,0 +1,30 @@
+"""
+Contains all the classes and logic related to orders, reading order files.
+"""
+class OrderProcessor:
+    """
+    An Order Processor is an object that is responsible for reading a order
+    file to create an Order object.
+
+    Factory Object?
+    """
+    def __init__(self):
+        pass
+
+    def read_order(self):
+        pass
+
+
+class Order:
+    """
+    An Order is an analogy for an business transaction that orders a variety
+    of items with appropriate business attributes like order number,
+    product ids.
+    """
+#  todo Add functionality
+# •	Order number
+# •	Product ID
+# •	Item - The type of item (Toy, StuffedAnimal and Candy).
+# •	Name of the item
+# •	A dictionary of product details. These details are the rest of the attributes of the item as specified in the excel sheet EXCEPT the name of the holiday — Easter, Christmas or Halloween.
+# •	The order should also contain a reference to the appropriate Factory object that can create this item.
