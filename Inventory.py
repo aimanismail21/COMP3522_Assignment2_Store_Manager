2f608b314759d715feff712864b37621f7857723
Michael Vu <mvu14@my.bcit.ca>
2020-03-14 09:08:28 -0700

    Created Toy, StuffedAnimal and Candy class

--- Inventory.py
+++ Inventory.py
@@ -6,6 +6,64 @@ Manages Toys, Stuffed Animals, and Candy objects.
 from abc import ABC
 
 class ItemFactory(ABC):
+    """
+    #todo
+    """
     pass
+
+class Toy(ABC):
+    """
+    Toy defines the interface that the ItemFactory is responsible to create.
+    """
+
+    def __init__(self, is_battery_operated, min_age, name, description, product_id):
+        """
+        Initialize a toy.
+        """
+        self.has_battery = is_battery_operated
+        self.min_age = min_age
+        self.name = name
+        self.description = description
+        self.product_id = product_id
+
+    pass
+
+class StuffedAnimal(ABC):
+    """
+    StuffedAnimal defines the interface that the ItemFactory is responsible to create.
+    """
+
+    stuffing_options = ['polyester', 'fiberfill', 'woll']
+    size_options = ['small', 'medium', 'large']
+    fabric_options = ['linen', 'cotton', 'acrylic']
+
+    def __init__(self, stuffing, size, fabric, name, description, product_id):
+        """
+        Initialize a Stuffed animal
+        """
+
+        self.stuffing = stuffing
+        self.size = size
+        self.fabric = fabric
+        self.name = name
+        self.description = description
+        self.product_id = product_id
+
+class Candy(ABC):
+    """
+    #todo
+    """
+
+    def __init__(self, has_nuts, has_lactose, name, description, product_id):
+        self.has_nuts = has_nuts
+        self.has_lactose = has_lactose
+        self.description = description
+        self.product_id = product_id
+    pass
+
+class SantasWorkShop(Toy):
+    """
+    #todo
+    """
 
 
