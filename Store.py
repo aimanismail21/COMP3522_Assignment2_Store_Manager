0f372fc6fe8d3f33acdfcc6a18fcdf27ef78ff7b
Aiman Ismail <aimanismail15@gmail.com>
2020-03-25 11:59:57 -0700

    Updated validate_order, and create_items, process_order, and menu_order_processing

--- Store.py
+++ Store.py
@@ -33,8 +33,7 @@ class Store:
         valid_order = True
         invalid_data_reasons = []
         if order.order_number in self.order_records:
-            valid_order = False
-            invalid_data_reasons.append("Order was processed previously.")
+            order.order_number = self._counter_of_records
         if order.holiday[0] != order.product_id[0] or order.item[0] != \
                 order.product_id[5]:
             valid_order = False
@@ -67,34 +66,20 @@ class Store:
         :param quantity: int, number of items to create
         :return: None
         """
-
-        try:
-            factory = order.factory()
-            number_to_create = self._number_of_items_to_create - quantity
-            for number_of_items_to_creates \
-                    in range(0, number_to_create):
-                if order.item == "Toy":
-                    toy = factory.create_toy(**order.product_details)
-                    yield toy
-                if order.item == "StuffedAnimal":
-                    stuffed_animal = factory.create_stuffed_animal(
-                        **order.product_details)
-                    yield stuffed_animal
-                if order.item == "Candy":
-                    candy = factory.create_candy(**order.product_details)
-                    yield candy
-
-        except Exception as e:
-            self.order_records[
-                order.order_number] = f"Order {order.order_number}, " \
-                                      f"Could not " \
-                                      f"process order as" \
-                                      f" data was corrupted," \
-                                      f" InvalidDataError -" \
-                                      f" {e}"
-
-        else:
-            pass
+        factory = order.factory()
+        number_to_create = self._number_of_items_to_create - quantity
+        for number_of_items_to_creates \
+                in range(0, number_to_create):
+            if order.item == "Toy":
+                toy = factory.create_toy(**order.product_details)
+                yield toy
+            if order.item == "StuffedAnimal":
+                stuffed_animal = factory.create_stuffed_animal(
+                    **order.product_details)
+                yield stuffed_animal
+            if order.item == "Candy":
+                candy = factory.create_candy(**order.product_details)
+                yield candy
 
     def daily_transaction_report(self):
         """
@@ -136,12 +121,17 @@ class Store:
             product_ledger = self.inventory[product_id]
             item_list = product_ledger['item']
             if product_ledger['quantity'] < quantity_requested:
-                unfulfilled_orders = product_ledger[
-                                         'quantity'] - quantity_requested
+                unfulfilled_orders = int(product_ledger['quantity']) - \
+                                     int(quantity_requested)
                 for item_to_remove in range(product_ledger['quantity']):
                     item_list.pop()
                 product_ledger['quantity'] = 0
-                return unfulfilled_orders
+                return abs(unfulfilled_orders)
+            if product_ledger['quantity'] > quantity_requested:
+                for item_to_remove in range(quantity_requested):
+                    item_list.pop()
+                product_ledger['quantity'] -= quantity_requested
+                return 0
 
     def check_inventory(self):
         """
@@ -172,10 +162,11 @@ class Store:
         for order in new_order_processing.create_order_from_order_file():
             valid = self.validate_order(order)
             if valid:
-                unfufilled_quantity = self.process_order(order.product_id,
-                                                         order.quantity,
-                                                         order.name)
-                for item in self.create_items(order, unfufilled_quantity):
-                    product_ledger = self.inventory[order.product_id]
-                    product_ledger['quantity'] += 1
-                    product_ledger['item'].append(item)
+                unfulfilled_quantity = self.process_order(order.product_id,
+                                                          order.quantity,
+                                                          order.name)
+                if unfulfilled_quantity > 0:
+                    for item in self.create_items(order, unfulfilled_quantity):
+                        product_ledger = self.inventory[order.product_id]
+                        product_ledger['quantity'] += 1
+                        product_ledger['item'].append(item)
